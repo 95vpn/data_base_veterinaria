7d66927cdbaf3132adc8146fb21e0a03069d8ad1
Johnny Herrera <jmjaramillo95@gmail.com>
2024-07-18 22:21:05 -0500

    veterinaria-controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const petRouter = require('./pet.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/pets', petRouter)
 
 module.exports = router;
